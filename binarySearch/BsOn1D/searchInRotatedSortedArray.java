e771e897860d8bf3e3f6fea1e4acd74d88fa7a8e
Shivam <shivamdnngsp@gmail.com>
2026-01-02 02:21:07 +0530

    Implement binary search in rotated sorted array

---
+++ binarySearch/BsOn1D/searchInRotatedSortedArray.java
@@ -0,0 +1,39 @@
+public class random {
+    static int search(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[low] <= arr[mid]) {
+
+                if (target >= arr[low] && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            }
+            else {
+
+                if (target > arr[mid] && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6,7,1,2,3,4,5};
+        int x = 7;
+        System.out.println(search(arr, x));
+    }
+}
