e5dbfc80cae0fdce59b784db103def49372395be
Shivam <shivamdnngsp@gmail.com>
2026-05-30 15:09:45 +0530

    Implement method to remove outer parentheses

---
+++ String[Basic-Medium]/Easy/RemoveOutermostParentheses/optimal.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+               if(count != 0){
+                sb.append(c);
+               }
+               count++;
+            } else {
+                count--;
+                if (count != 0) {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
