cf6ed0ed8fe845177746e471c5d88714bdeb261e
Shivam <shivamdnngsp@gmail.com>
2026-07-13 17:31:58 +0530

    Time: 1 ms (97.98%), Space: 47.1 MB (56.23%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,49 +1,27 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> l = new ArrayList<>();
-        if(newInterval.length == 0){
-           return intervals;
-        }
-        if(intervals.length == 0){
-            return new int[][] {newInterval};
-        }
-
-
-
-        boolean used = false;
-    
-         int i = 0;
-         while(i<intervals.length){
-                if(!used){
-                    if(newInterval[1]<intervals[i][0]){
-                        used = true;
-                        l.add(newInterval);
-                    }
-                   else if(newInterval[0]<= intervals[i][1]){
-                    intervals[i][1] = Math.max(intervals[i][1],newInterval[1]);
-                    intervals[i][0] = Math.min(intervals[i][0],newInterval[0]);
-                    used = true;
-                    l.add(intervals[i]);
-                    continue;
-                    
-                   }
-                }
-                
-                    if(l.isEmpty() || l.get(l.size()-1)[1]< intervals[i][0]){
-                        l.add(intervals[i]);
-                    }else{
-                        l.get(l.size()-1)[1] = Math.max(l.get(l.size()-1)[1] , intervals[i][1]);
-                    }
-                
-
-            i++;
-         }
-if(!used){
-    l.add(newInterval);
-}
          
-         return l.toArray(new int[l.size()][]);
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < newInterval[0]){
+         l.add(intervals[i]);
+         i++;
+        }
 
+        while(i<intervals.length && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+        l.add(newInterval);
+        
+        while(i<intervals.length){
+          l.add(intervals[i]);
+          i++;
+        }
+
+       
+ return l.toArray(new int[l.size()][]);
         
     }
 }
