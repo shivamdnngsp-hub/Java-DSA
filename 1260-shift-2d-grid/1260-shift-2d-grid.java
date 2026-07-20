52aa572871d5f94a2fa7b14137558ae3e07e42bc
Shivam <shivamdnngsp@gmail.com>
2026-07-20 14:50:56 +0530

    Time: 6 ms (55.4%), Space: 47.5 MB (8.97%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,44 +1,29 @@
 class Solution {
 
-    void rotate(int[] arr,int i,int j){
-        while(i<j){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
-    }
-    
-
 
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        List<List<Integer>>  ans = new ArrayList<>();
-
-        int[] arr = new int[grid.length * grid[0].length];
-        int x = 0;
-        for(int i = 0;i<grid.length;i++){
-            for(int j = 0;j<grid[0].length;j++){
-                arr[x] = grid[i][j];
-                x++;
-            } 
+    
+       int[][] arr = new int[grid.length][grid[0].length];
+       for(int i = 0;i<grid.length;i++){
+        for(int j = 0;j<grid[0].length;j++){
+            int index = i*grid[0].length +j;
+            int newIndex = (index+k)%(grid.length*grid[0].length);
+            int x = newIndex/grid[0].length;
+            int y = newIndex%grid[0].length;
+            arr[x][y] = grid[i][j];
         }
+       }
 
-        k = k%arr.length;
-        rotate(arr,0,arr.length-1);
-        rotate(arr,0,k-1);
-        rotate(arr,k,arr.length-1);
-        x = 0;
-        for(int i = 0;i<grid.length;i++){
-            List<Integer> l = new ArrayList<>();
-            for(int j = 0;j<grid[0].length;j++){
-                l.add(arr[x]);
-                x++;
-            }
-            ans.add(l);
-        }
-       
-      return ans;
-        
+List<List<Integer>> ans = new ArrayList<>();
+for(int[] row : arr){
+List<Integer> l = new ArrayList<>();
+for(int i = 0;i<row.length;i++){
+    l.add(row[i]);
+}
+ans.add(l);
+}
+
+
+      return ans;  
     }
 }
