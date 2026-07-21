476fd227f84f2af4695dffcef0366993190162e8
Shivam <shivamdnngsp@gmail.com>
2026-07-21 14:46:15 +0530

    Time: 15 ms (10.73%), Space: 43.9 MB (76.9%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+   
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Queue<TreeNode> q  = new LinkedList<>();
+        HashMap<TreeNode,TreeNode> mpp = new HashMap<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size  = q.size();
+            for(int i = 0;i<size;i++){
+                TreeNode node = q.remove();
+                 if(node.left != null){
+                   mpp.put(node.left,node);
+                   q.offer(node.left);
+                 }
+                 if(node.right != null){
+                   mpp.put(node.right,node);
+                   q.offer(node.right);
+                 }
+            }
+        }
+
+        List<TreeNode> visited = new ArrayList<>();
+        visited.add(target);
+        q.offer(target);
+        int dis = 0;
+        while(!q.isEmpty()){
+            if(dis == k){
+                break;
+            }
+            int size = q.size();
+
+            for(int i = 0;i<size;i++){
+
+
+           TreeNode node = q.remove();
+           if(node.left != null && !visited.contains(node.left)){
+                   q.offer(node.left);
+                   visited.add(node.left);
+            }
+            if(node.right != null && !visited.contains(node.right)){
+                   q.offer(node.right);
+                   visited.add(node.right);
+            }
+            if(mpp.containsKey(node) && !visited.contains(mpp.get(node))){
+                q.offer(mpp.get(node));
+                visited.add(mpp.get(node));
+            }
+
+            }
+
+          dis++;
+        }
+
+List<Integer> ans = new ArrayList<>();
+while(!q.isEmpty()){
+    ans.add(q.remove().val);
+}
+
+
+return ans;
+        
+    }
+}
