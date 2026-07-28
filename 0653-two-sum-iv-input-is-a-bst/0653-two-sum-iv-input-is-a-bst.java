e9c82a197d7af241604c208573de1fd0bcf048fd
Shivam <shivamdnngsp@gmail.com>
2026-07-28 17:06:00 +0530

    Time: 2 ms (98.18%), Space: 47.1 MB (85.27%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,31 +14,64 @@
  * }
  */
 class Solution {
-    List<Integer> l = new ArrayList<>();
-    void helper(TreeNode node){
-        if(node == null){
-            return;
+
+        Stack<TreeNode> st2 = new Stack<>();
+        Stack<TreeNode> st1 = new Stack<>();
+
+
+
+    TreeNode nextl(){
+     TreeNode node = st1.pop();
+     if(node.right != null){
+        TreeNode right = node.right;
+        while(right != null){
+            st1.push(right);
+            right = right.left;
         }
-
-        helper(node.left);
-        l.add(node.val);
-        helper(node.right);
-
-      return;
+     }
+     return node;
     }
-    public boolean findTarget(TreeNode root, int k) {
-        helper(root);
-        int i = 0;
-        int j = l.size() -1;
-        while(i<j){
-            if(l.get(i) + l.get(j) == k){
-                return true;
-            }else if(l.get(i) + l.get(j)>k){
-                j--;
-            }else{
-                i++;
-            }
+
+
+     TreeNode nextr(){
+     TreeNode node = st2.pop();
+     if(node.left != null){
+        TreeNode left = node.left;
+        while(left != null){
+            st2.push(left);
+            left  = left.right;
         }
-        return false;
+     }
+     return node;
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        TreeNode node = root;
+        while(node != null){
+            st1.push(node);
+            node = node.left;
+        }
+        node = root;
+        while(node != null){
+            st2.push(node);
+            node = node.right;
+        }
+int left = nextl().val;
+int right =  nextr().val;
+
+while (left < right) {
+    int sum = left + right;
+    if (sum == k){
+        return true;
+    }else if (sum < k){
+        left = nextl().val;
+    }else{
+         right = nextr().val;  
+    }
+           
+}
+
+
+return false;
     }
 }
