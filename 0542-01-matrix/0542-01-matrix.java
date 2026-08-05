6cf710777bbe10c43130e771f8800c6a3cefbe3e
Shivam <shivamdnngsp@gmail.com>
2026-08-05 14:24:51 +0530

    Time: 17 ms (17.87%), Space: 49 MB (59.34%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+      class Pair {
+        int row;
+        int col;
+        int level;
+
+        Pair(int row, int col,int level) {
+            this.row = row;
+            this.col = col;
+            this.level = level;
+        }
+    }
+
+    public int[][] updateMatrix(int[][] mat) {
+         boolean[][] visited = new boolean[mat.length][mat[0].length];
+         Queue<Pair> q= new LinkedList<>();
+         int[][] ans = new int[mat.length][mat[0].length];
+         for(int i = 0;i<mat.length;i++){
+            for(int j = 0;j<mat[0].length;j++){
+                if(mat[i][j] == 0){
+                    q.offer(new Pair(i,j,0));
+                    visited[i][j] = true;
+                }
+            }
+         }
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+         while(!q.isEmpty()){
+            Pair p = q.remove();
+            for(int i = 0;i<4;i++){
+                int nr = dr[i] + p.row;
+                int nc = dc[i] + p.col;
+             if(nr >= 0 && nr < mat.length && nc >= 0 && nc < mat[0].length && !visited[nr][nc]){
+                 ans[nr][nc] = p.level +1;
+                 visited[nr][nc] = true;
+                 q.offer(new Pair(nr,nc,p.level +1));
+             }
+            }
+         }
+
+
+         return ans;
+    }
+}
