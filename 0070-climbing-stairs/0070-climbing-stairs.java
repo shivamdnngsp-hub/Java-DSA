8cc7606a7484f150e92170e6d9ece4bc5a9a6c7c
Shivam <shivamdnngsp@gmail.com>
2026-09-03 15:14:27 +0530

    Time: 0 ms (100%), Space: 42 MB (73.14%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    int helper(int i,int n,int[] dp){
+        if(i == n){
+            return 1;
+        }
+         if(i>n){
+             return 0;
+         }
+         if(dp[i] != -1) return dp[i];
+
+            return dp[i]  =  helper(i+1,n,dp) + helper(i+2,n,dp);
+    }
+
+
+    
+    public int climbStairs(int n) {
+        int count = 0;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(0,n,dp);
+    }
+}
