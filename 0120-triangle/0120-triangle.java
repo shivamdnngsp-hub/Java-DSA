bc50ffd10ae52dee23c7e76a0b794b5e2642f85f
Shivam <shivamdnngsp@gmail.com>
2026-09-18 16:50:08 +0530

    Time: 4 ms (40.7%), Space: 45 MB (98.15%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,24 +1,24 @@
 class Solution {
     
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] dp = new int[triangle.size()][triangle.size()];
-        dp[0][0] = triangle.get(0).get(0);
+        int[] dp = new int[triangle.size()];
+        dp[0] = triangle.get(0).get(0);
         for(int i = 1;i<triangle.size();i++){
-            for(int j = 0;j<triangle.get(i).size();j++){
-              if(j == 0){
-                dp[i][j] = triangle.get(i).get(j) + dp[i-1][j];
-              }else if(j == triangle.get(i).size()-1){
-                dp[i][j] = triangle.get(i).get(j) + dp[i-1][j-1];
-              }else{
-                dp[i][j] = triangle.get(i).get(j)  + Math.min(dp[i-1][j-1],dp[i-1][j]);
-              }
+            for(int j = triangle.get(i).size()-1;j>=0;j--){
+                if(j == 0){
+                    dp[j] = triangle.get(i).get(j) + dp[j];
+                }else if(j == triangle.get(i).size()-1){
+                    dp[j] = triangle.get(i).get(j) + dp[j-1];
+                }else{
+                    dp[j] = triangle.get(i).get(j)  + Math.min(dp[j],dp[j-1]);
+                }
             }
         }
-        int ans = Integer.MAX_VALUE;
-        for(int j = 0; j < triangle.get(triangle.size()-1).size(); j++) {
-            ans = Math.min(ans, dp[triangle.size()-1][j]);
-        }
 
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0;i<triangle.size();i++){
+           ans = Math.min(ans,dp[i]);
+        }
         return ans;
     }
 }
